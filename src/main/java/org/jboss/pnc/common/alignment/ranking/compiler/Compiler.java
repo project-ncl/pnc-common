9c73a00038a410c0c655057ff5d50998f0554a48
Nick Cross <ncross@redhat.com>
2026-02-16 17:09:43 +0000

    Revert "[Alignment 2.0] implement Qualifier Compiler"
    This reverts commit 6801cfe12e8aa23d1b3a79a7200231ed518e10df.

--- src/main/java/org/jboss/pnc/common/alignment/ranking/compiler/Compiler.java
+++
@@ -1,7 +0,0 @@
-package org.jboss.pnc.common.alignment.ranking.compiler;
-
-import org.jboss.pnc.common.alignment.ranking.parser.InternalNode;
-
-public interface Compiler<T> {
-    T compile(InternalNode rootNode);
-}
